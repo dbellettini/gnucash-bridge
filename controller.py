9424197dc575d75e89681ef3604d4835fa65c813
Davide Bellettini <davide@bellettini.me>
2015-09-04 20:44:09 +0200

    Better account map

--- controller.py
+++ controller.py
@@ -1,20 +1,38 @@
 from bottle import route
+from collections import OrderedDict
 from uuid import UUID
 
 def account_uuid(account):
     return str(UUID(account.GetGUID().to_string()))
 
+
+def map_account(account):
+    dict = OrderedDict([
+        ('id', account_uuid(account)),
+        ('name', account.name)
+    ])
+
+    children = []
+    for child in account.get_children_sorted():
+        children.append(map_account(child))
+
+    if len(children) > 0:
+        dict['children'] = children;
+
+    return dict
+
+def map_root_account(root_account):
+    mapped = {'items': []}
+
+    for child in root_account.get_children_sorted():
+        mapped['items'].append(map_account(child))
+
+    return mapped
+
 class AccountController:
-    @route('/accounts')
+    @route('/accounts', 'GET')
     def list():
         root_account = AccountController.session.book.get_root_account()
 
-        accounts = []
-        for account in root_account.get_children_sorted():
-            accounts.append({
-                'id': account_uuid(account),
-                'name': account.name
-            })
-
-        return {'items': accounts}
+        return map_root_account(root_account)
 
